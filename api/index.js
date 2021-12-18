98c6b94d8af310ac5daa00aa4baaa1c10140ac05
Rafael Tomás <rafaeltomassouza@gmail.com>
2021-12-17 23:39:02 -0300

    add o config

--- api/index.js
+++ api/index.js
@@ -1,1 +1,9 @@
-npm
+const express = require('express')
+const bodyParser = require('body-parser')
+const config = require('config')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.listen(config.get('api.porta'), ()=> console.log("API esta rodando"))
