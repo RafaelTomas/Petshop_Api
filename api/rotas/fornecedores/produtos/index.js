6b26bb1872bc42a23241cbda82c9328b85747cfe
Rafael Tomás <rafaeltomassouza@gmail.com>
2021-12-24 00:09:08 -0300

    Rota post declared

--- api/rotas/fornecedores/produtos/index.js
+++ api/rotas/fornecedores/produtos/index.js
@@ -1,5 +1,6 @@
 const roteador = require('express').Router({mergeParams: true})
 const tabela = require('./TabelaProduto')
+const Produto = require('./Produtos')
 
 roteador.get('/', async (req, res) => {
     const produtos =  await tabela.listar(req.params.idFornecedor)
@@ -8,5 +9,17 @@ roteador.get('/', async (req, res) => {
     )
 })
 
+roteador.post('/', async (req, res) =>{
+    const idFornecedor = req.params.idFornecedor
+    const corpo = req.body
+    const dados = Object.assign({}, corpo,{ fornecedor: idFornecedor})
+    const produto = new Produto(dados)
+    await produto.criar()
+    res.status(201)
+    res.send(produto)
+})
+
+const roteadorReclemacoes = require('./reclamacoes')
+roteador.use('/:idProduto/reclamacoes', roteadorReclemacoes)
 
 module.exports = roteador
